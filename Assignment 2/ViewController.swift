d23810125596a4048baab61641f34d1edf5f685d
Robert Onuma <ronuma1234@gmail.com>
2021-12-07 14:15:17 +0000

    Initial Commit

---
+++ Assignment 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment 2
+//
+//  Created by Robert Onuma on 07/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
