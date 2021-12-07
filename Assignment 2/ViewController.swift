7436541f9b512126a9ee77057fbade4f1975d64f
Robert Onuma <ronuma1234@gmail.com>
2021-12-07 15:28:28 +0000

    Successfully retrieved data using JSON and it successfully displayed on the table.

--- Assignment 2/ViewController.swift
+++ Assignment 2/ViewController.swift
@@ -9,23 +9,50 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
+    var reports:ArtOnCampus? = nil
     
     @IBOutlet weak var map: MKMapView!
     
     @IBOutlet weak var myTable: UITableView!
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return reports?.campusart.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let myCell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        myCell.textLabel?.text = reports?.campusart[indexPath.row].title ?? "no title"
+        
+        return myCell
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let url = URL(string: "https://cgi.csc.liv.ac.uk/~phil/Teaching/COMP228/artworksOnCampus/data.php?class=campusart") {
+              let session = URLSession.shared
+              session.dataTask(with: url) { (data, response, err) in
+                  guard let jsonData = data else {
+                      return
+                  }
+                  do {
+                      let decoder = JSONDecoder()
+                      let reportList = try decoder.decode(ArtOnCampus.self, from: jsonData)
+                      self.reports = reportList
+                      DispatchQueue.main.async {
+                          self.updateTheTable()
+                      }
+                  } catch let jsonErr {
+                      print("Error decoding JSON", jsonErr)
+                  }
+             }.resume()
+            //print(reports)
+          }
+    }
+    
+    func updateTheTable() {
+        myTable.reloadData()
     }
 
 
---
+++ Assignment 2/assignmentDataModel.swift
@@ -0,0 +1,29 @@
+//
+//  assignmentDataModel.swift
+//  Assignment 2
+//
+//  Created by Robert Onuma on 07/12/2021.
+//
+
+import Foundation
+
+struct Artwork : Decodable {
+    let id : String
+    let title : String
+    let artist : String
+    let yearOfWork : String
+    let type : String?
+    let Information : String
+    let lat : String
+    let long : String
+    let location : String
+    let locationNotes : String
+    let ImagefileName : String
+    let thumbnail : URL?
+    let lastModified : String
+    let enabled : String
+}
+
+struct ArtOnCampus: Decodable {
+    let campusart: [Artwork]
+}
